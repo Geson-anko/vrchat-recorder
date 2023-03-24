bdb10a66ca2126fa156d1993c5d0057afa262b5d
GesonAnko <59220704+Geson-anko@users.noreply.github.com>
2023-03-24 21:31:03 +0900

    ADD Constants

---
+++ tests/test_data_constants.py
@@ -0,0 +1,13 @@
+from vrchat_recorder.data_constants import CSVHeaderNames, FileExtensions
+
+def test_CSVHeaderNames():
+    assert CSVHeaderNames.TIMESTAMP == "timestamp"
+    assert CSVHeaderNames.PARAMETER_NAME == "parameter_name"
+    assert CSVHeaderNames.DATA_TYPE == "data_type"
+    assert CSVHeaderNames.VALUE == "value"
+
+def test_FileExtensions():
+    assert FileExtensions.VRCREC == "vrcrec"
+    assert FileExtensions.CSV == "csv"
+    assert FileExtensions.OSCFEEDBACK == "oscfb"
+    assert FileExtensions.CONTROLLER == "ctrlr"
---
+++ vrchat_recorder/data_constants.py
@@ -0,0 +1,18 @@
+"""This class contains all the data constants used in the program."""
+
+class CSVHeaderNames:
+    """This class contains all the header names for the CSV files."""
+
+    TIMESTAMP = "timestamp"
+    PARAMETER_NAME = "parameter_name"
+    DATA_TYPE = "data_type"
+    VALUE = "value"
+
+
+class FileExtensions:
+    """This class contains all the file extensions used in the program."""
+
+    VRCREC = "vrcrec"
+    CSV = "csv"
+    OSCFEEDBACK = "oscfb"
+    CONTROLLER = "ctrlr"
