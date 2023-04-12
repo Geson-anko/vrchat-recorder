d5376ad2368ec341be13135000521685814717cd
GesonAnko <59220704+Geson-anko@users.noreply.github.com>
2023-04-12 20:41:55 +0900

    ADD mic and speaker parser

--- tests/test_argument_parser.py
+++ tests/test_argument_parser.py
@@ -21,6 +21,8 @@ def test_default_values(parser: ArgumentParser):
     assert not args.no_osc_feedback
     assert not args.no_gamepad
     assert not args.no_obs
+    assert not args.no_mic
+    assert not args.no_speaker
 
     assert args.vrchat_osc_ip == "localhost"
     assert args.vrchat_osc_port == 9001
@@ -30,6 +32,20 @@ def test_default_values(parser: ArgumentParser):
     assert args.obs_websocket_ip == "localhost"
     assert args.obs_websocket_port == 4444
     assert args.obs_websocket_password == "password"
+
+    assert args.mic_device_name is None
+    assert args.mic_sample_rate == 44100
+    assert args.mic_block_size == 4096
+    assert args.mic_channels == 1
+    assert args.mic_flush_interval == 100
+    assert args.mic_subtype == "PCM_16"
+
+    assert args.speaker_device_name is None
+    assert args.speaker_sample_rate == 44100
+    assert args.speaker_block_size == 4096
+    assert args.speaker_channels == 2
+    assert args.speaker_flush_interval == 100
+    assert args.speaker_subtype == "PCM_16"
 
 
 def test_custom_values(parser: ArgumentParser):
@@ -44,6 +60,8 @@ def test_custom_values(parser: ArgumentParser):
         "--no_osc_feedback",
         "--no_gamepad",
         "--no_obs",
+        "--no_mic",
+        "--no_speaker",
         "--vrchat_osc_ip",
         "192.168.1.1",
         "--vrchat_osc_port",
@@ -58,6 +76,30 @@ def test_custom_values(parser: ArgumentParser):
         "5000",
         "--obs_websocket_password",
         "custom_password",
+        "--mic_device_name",
+        "custom_mic_device_name",
+        "--mic_sample_rate",
+        "48000",
+        "--mic_block_size",
+        "2048",
+        "--mic_channels",
+        "2",
+        "--mic_flush_interval",
+        "200",
+        "--mic_subtype",
+        "PCM_24",
+        "--speaker_device_name",
+        "custom_speaker_device_name",
+        "--speaker_sample_rate",
+        "48000",
+        "--speaker_block_size",
+        "2048",
+        "--speaker_channels",
+        "2",
+        "--speaker_flush_interval",
+        "200",
+        "--speaker_subtype",
+        "PCM_24",
     ]
 
     args = parser.parse_args(custom_args)
@@ -70,6 +112,8 @@ def test_custom_values(parser: ArgumentParser):
     assert args.no_osc_feedback
     assert args.no_gamepad
     assert args.no_obs
+    assert args.no_mic
+    assert args.no_speaker
 
     assert args.vrchat_osc_ip == "192.168.1.1"
     assert args.vrchat_osc_port == 8000
@@ -79,3 +123,17 @@ def test_custom_values(parser: ArgumentParser):
     assert args.obs_websocket_ip == "10.0.0.1"
     assert args.obs_websocket_port == 5000
     assert args.obs_websocket_password == "custom_password"
+
+    assert args.mic_device_name == "custom_mic_device_name"
+    assert args.mic_sample_rate == 48000
+    assert args.mic_block_size == 2048
+    assert args.mic_channels == 2
+    assert args.mic_flush_interval == 200
+    assert args.mic_subtype == "PCM_24"
+
+    assert args.speaker_device_name == "custom_speaker_device_name"
+    assert args.speaker_sample_rate == 48000
+    assert args.speaker_block_size == 2048
+    assert args.speaker_channels == 2
+    assert args.speaker_flush_interval == 200
+    assert args.speaker_subtype == "PCM_24"
