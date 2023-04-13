ac4d21e50ed575bb9e83cf9d7ef863b873cfd969
GesonAnko <59220704+Geson-anko@users.noreply.github.com>
2023-04-13 21:34:40 +0900

    Format by pre-commit

--- demos/audio_recording.py
+++ demos/audio_recording.py
@@ -1,8 +1,9 @@
-from vrchat_recorder.audio import MicRecorder, SpeakerRecorder
-
-from pathlib import Path
 import time
+from pathlib import Path
+
 import soundcard as sc
+
+from vrchat_recorder.audio import MicRecorder, SpeakerRecorder
 
 output_dir = Path(__file__).parent / "output"
 output_dir.mkdir(exist_ok=True)
@@ -11,20 +12,11 @@ mic_output_path = output_dir / "demo_mic_recording_%Y-%m-%d-%H-%M-%S-%f.wav"
 speaker_output_path = output_dir / "demo_speaker_recording_%Y-%m-%d-%H-%M-%S-%f.wav"
 
 
-
 mic_recorder = MicRecorder(
-    str(mic_output_path), 
-    sc.default_microphone().name, 
-    sample_rate=44100, 
-    block_size=4096, 
-    num_channels=1
+    str(mic_output_path), sc.default_microphone().name, sample_rate=44100, block_size=4096, num_channels=1
 )
 speaker_recorder = SpeakerRecorder(
-    str(speaker_output_path), 
-    sc.default_speaker().name,
-    sample_rate=44100, 
-    block_size=4096, 
-    num_channels=2
+    str(speaker_output_path), sc.default_speaker().name, sample_rate=44100, block_size=4096, num_channels=2
 )
 
 
