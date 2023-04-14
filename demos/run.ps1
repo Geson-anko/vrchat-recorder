f2a637df00a4ae823a4450c54a9f495527a7190b
GesonAnko <59220704+Geson-anko@users.noreply.github.com>
2023-04-14 22:19:01 +0900

    デフォルトの起動コマンドを指定したスクリプトの作成 #8

---
+++ demos/run.ps1
@@ -0,0 +1,10 @@
+& python -m vrchat_recorder `
+    -d "$env:USERPROFILE\Desktop\VRChat Recordings" `
+    --mic "Headset Microphone (Oculus Virtual Audio Device)" `
+    --speaker "ヘッドホン (Oculus Virtual Audio Device)" `
+    --no_gamepad ` # If you don't want to record gamepad info.
+    # --no_obs ` # If you don't want to record by OBS.
+    # --no_osc_feedback ` # If you don't want to record OSC feedback.
+    # --no_mic ` # If you don't want to record mic.
+    # --no_speaker ` # If you don't want to record speaker.
+    # --no_vr ` # If you don't want to record VR info.
